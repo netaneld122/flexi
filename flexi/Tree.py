a417b9460e7a8ef322d1e31ae46a86f6e1292b05
Netanel Dziubov <netaneld122@gmail.com>
2016-02-26 02:08:14 +0200

    Added magic methods implementation and some tests

--- flexi/Tree.py
+++ flexi/Tree.py
@@ -4,7 +4,7 @@ import collections
 class Tree(object):
 
     def __init__(self):
-        # This is the way to use a member without causing an infinite recursive call (due to __setattr__)
+        # This is the proper way to use a member without causing an infinite recursive call (due to __setattr__)
         self.__dict__['data'] = collections.OrderedDict()
 
     def __getattr__(self, key):
@@ -17,10 +17,22 @@ class Tree(object):
         data = self.__dict__['data']
         data[key] = value
 
-    def __getitem__(self, item):
+    def __delattr__(self, key):
         data = self.__dict__['data']
-        return data[item]
+        del data[key]
+
+    def __getitem__(self, key):
+        data = self.__dict__['data']
+        return data[key]
 
     def __setitem__(self, key, value):
         data = self.__dict__['data']
         data[key] = value
+
+    def __iter__(self):
+        data = self.__dict__['data']
+        return data.iteritems()
+
+    def __contains__(self, key):
+        data = self.__dict__['data']
+        return key in data
--- test/test_tree.py
+++ test/test_tree.py
@@ -4,8 +4,30 @@ import flexi
 
 class TestTree(unittest.TestCase):
 
-    def test_sanity(self):
+    def test_list(self):
         root = flexi.Tree()
-        root.key.int = 1
-        root.key.double = 2.0
-        root.key.string = 'string'
+        root.tree.tree2.list = [1, 2, 3]
+        self.assertListEqual(root.tree.tree2.list, [1, 2, 3])
+
+    def test_get_item(self):
+        root = flexi.Tree()
+        root.tree.int = 1
+        self.assertEqual(root.tree['int'], 1)
+
+    def test_recursive_scanning(self):
+        root = flexi.Tree()
+        root.tree.int = 1
+        root.tree.double = 2.0
+        root.tree.string = 'string'
+        root.tree.tree2.list = [1, 2, 3]
+
+        def recurse(tree):
+            for key, value in tree:
+                if isinstance(value, flexi.Tree):
+                    recurse(value)
+        recurse(root)
+
+    def test_contains(self):
+        root = flexi.Tree()
+        root.tree.string = 'string'
+        self.assertIn('string', root.tree)
